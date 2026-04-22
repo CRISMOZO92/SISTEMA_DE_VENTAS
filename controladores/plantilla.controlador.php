649daf5420680de05f5427576d7d2bc72b3792e8
Hermes <hgil@sistran.com.co>
2026-04-22 18:58:43 -0500

    Primer commit

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class ControladorPlantilla{
+
+    static public function ctrplantilla(){
+
+    include "vistas/plantilla.php";
+
+
+    }
+
+}
+
+
+
+
+?>
---
+++ vistas/modulos/menu.php
@@ -0,0 +1,85 @@
+ <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Cristian Mozo</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MENU</li>
+        <li>
+          <a href="usuarios">
+            <i class="fa fa-th"></i> <span>usuarios</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="categorias">
+            <i class="fa fa-th"></i> <span>categorias</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="clientes">
+            <i class="fa fa-th"></i> <span>clientes</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="ventas">
+            <i class="fa fa-th"></i> <span>ventas</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="productos">
+            <i class="fa fa-th"></i> <span>productos</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="vistas/index.html"><i class="fa fa-circle-o"></i> Crear ventas</a></li>
+            <li><a href="vistas/index2.html"><i class="fa fa-circle-o"></i> Reportes</a></li>
+          </ul>
+        </li>
+        
+        
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
