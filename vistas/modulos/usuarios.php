2a38adfd109dacb8df1c3721e81772c725a6b334
Hermes <hgil@sistran.com.co>
2026-04-24 22:17:14 -0500

    Se creo la tabla para datos de la ventana o el modulo usuarios con los datos como nombre, usuario, foto, perfil, estado y acciones. Adicionalmente se creo ell boton de Agregar usuario

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -3,13 +3,13 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Usuarios
-        <small>it all starts here</small>
+        Administrar Usuarios
+        
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+       
+        <li class="active">Administrar Usuarios</li>
       </ol>
     </section>
 
@@ -19,26 +19,58 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+
+        <button type="button" class="btn btn-primary" data-toggle="modal"
+                    data-target="#modalAgregarUsuario">
+              agregar usuario</button>
+          
+
+          
         </div>
         <div class="box-body">
           Start creating your amazing application!
+
+          <table class="table table-bordered table-striped dt-responsive tablas" whidth="100%">
+
+          <thead>
+            <tr>
+
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Usuario</th>
+              <th>Foto</th>
+              <th>Perfil</th>
+              <th>estado</th>
+              <th>Acciones</th>
+
+              
+            </tr>
+
+
+
+          </thead>
+
+
+          <tbody>
+
+
+
+          </tbody>
+
+
+
+          </table>
+
+
+
+
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+        
+       
       </div>
-      <!-- /.box -->
+     
 
     </section>
     <!-- /.content -->
