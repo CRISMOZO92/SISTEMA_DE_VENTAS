75b141a88e50a589357444679f53c8c0c03becd7
Hermes <hgil@sistran.com.co>
2026-06-04 21:05:32 -0500

    se configuro el xdebug para validar errores de aplicacion y se arreglo la linea de codigo             if(isset($_FILES["nuevaFoto"]["tmp_name"]) && $_FILES["nuevaFoto"]["tmp_name"] != ""){ para que al momento de agregar el usuario y no quiera agregarle una foto no me arroje error

--- controladores/usuario.controlador.php
+++ controladores/usuario.controlador.php
@@ -31,7 +31,7 @@ class ControladorUsuarios {
             $ruta="";
 
 
-            if(isset($_FILES["nuevaFoto"]["tmp_name"])){
+            if(isset($_FILES["nuevaFoto"]["tmp_name"]) && $_FILES["nuevaFoto"]["tmp_name"] != ""){
 
 
                 list($ancho,$alto)=getimagesize($_FILES["nuevaFoto"]["tmp_name"]);
